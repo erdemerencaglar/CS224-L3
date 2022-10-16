81e6a680595a0bdd262dae5525cd3b7eb1af5b4b
erdemerencaglar <caglarerdemeren@gmail.com>
2022-10-16 20:29:38 +0300

    lab 1 finished

--- lab.asm
+++ lab.asm
@@ -7,11 +7,9 @@
     line: .asciiz "\n"
 
 .text
-    # TODO: any number = 0 check
+
     main:
-
-        func:
-
+    
         li $v0, 4
         la $a0, promptDividend
         syscall
@@ -20,6 +18,7 @@
         syscall
 
         move $s0, $v0 # dividend
+
         beq $s0, $zero, exit
 
         li $v0, 4
@@ -30,12 +29,13 @@
         syscall
 
         move $s1, $v0 # divisor
+
         beq $s1, $zero, exit
 
         move $a0, $s0 # dividend
         move $a1, $s1 # divisor
 
-        addi $s2, $zero, 0
+        addi $s5, $zero, 0
 
         jal recursiveDivision
 
@@ -45,7 +45,7 @@
             syscall
 
             li $v0, 1
-            move $a0, $s2
+            move $a0, $s5
             syscall
 
             li $v0, 4
@@ -60,23 +60,53 @@
  
     recursiveDivision:
 
-        addi $s2, $s2, 1
+        addi $sp, $sp, -12
+        # sw $s5, 12($sp)
+        sw $s1, 8($sp)
+        sw $s0, 4($sp)
+        sw $ra, 0($sp)
+
+        li $v0, 1
+        # beq $s0, $zero, recursionDone
+        blt $s0, $s1, recursionDone
+        beq $s1, $zero, recursionDone
+
         move $s0, $a0 # dividend
         move $s1, $a1 # divisor
 
         sub $s0, $s0, $s1
-
         move $a0, $s0
         move $a1, $s1
+        jal recursiveDivision
 
-        sub $s4, $s0, $s1
-        # addi $s4, $s0, -3
-        bge $s4, $zero, recursiveDivision
+        addi $s5, $s5, 1
 
-        beq $s1, $zero, exitRec
-        beq $s0, $zero, exitRec
+        recursionDone:
+            lw $ra, 0($sp)
+            lw $s0, 4($sp)
+            lw $s1, 8($sp)
+            # lw $s5, 12($sp)
+            addi $sp, $sp, 12
 
-        jr $ra
+            jr $ra
+
+        # addi $s2, $s2, 1
+        # move $s0, $a0 # dividend
+        # move $s1, $a1 # divisor
+
+        # sub $s0, $s0, $s1
+
+        # move $a0, $s0
+        # move $a1, $s1
+
+        # sub $s4, $s0, $s1
+        # # addi $s4, $s0, -3
+        # bge $s4, $zero, recursiveDivision
+
+        # beq $s1, $zero, exitRec
+        # beq $s0, $zero, exitRec
+
+        # jr $ra
 
     menu: 
         li $v0, 4
@@ -88,5 +118,5 @@
 
         move $s3, $v0
 
-        beq $s3, 1, func
+        beq $s3, 1, main
         beq $s3, 2, exit
